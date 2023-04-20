b5ffd7e1cdc68902f41adabc1bc80e4e533ae119
rashirikun <92330721+rashirikun@users.noreply.github.com>
2023-04-20 10:56:20 -0700

    Update config.js

--- config.js
+++ config.js
@@ -32,7 +32,7 @@
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can Change this number //
+  gg = "19543371477";   // You can Change this number //
 }
 
 // -------------------------------------------------------------- //
